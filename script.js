57ff0933ac5549510f6204710674cf143de8e756
Yago <yago.qpio@sempreceub.com>
2024-08-20 09:08:13 -0300

    validação cpf

--- script.js
+++ script.js
@@ -15,11 +15,45 @@ document.getElementById('infoForm').addEventListener('submit', function(event) {
         return;
     }
 
-    if (!/^\d{11}$/.test(cpf)) {
-        alert('O CPF deve conter exatamente 11 dígitos numéricos.');
+    if (!validaCPF(cpf)) {
+        alert('CPF inválido.');
         return;
     }
 
     // Se todos os campos estão válidos, você pode prosseguir com o envio do formulário
     alert('Formulário enviado com sucesso!');
 });
+
+function validaCPF(cpf) {
+    cpf = cpf.replace(/[^\d]/g, ''); // Remove caracteres não numéricos
+
+    if (cpf.length !== 11 || /^(\d)\1+$/.test(cpf)) {
+        return false; // Verifica se o CPF tem 11 dígitos ou é uma sequência repetida
+    }
+
+    let soma = 0;
+    let resto;
+
+    for (let i = 1; i <= 9; i++) {
+        soma += parseInt(cpf.substring(i - 1, i)) * (11 - i);
+    }
+
+    resto = (soma * 10) % 11;
+    if (resto === 10 || resto === 11) resto = 0;
+    if (resto !== parseInt(cpf.substring(9, 10))) {
+        return false;
+    }
+
+    soma = 0;
+    for (let i = 1; i <= 10; i++) {
+        soma += parseInt(cpf.substring(i - 1, i)) * (12 - i);
+    }
+
+    resto = (soma * 10) % 11;
+    if (resto === 10 || resto === 11) resto = 0;
+    if (resto !== parseInt(cpf.substring(10, 11))) {
+        return false;
+    }
+
+    return true; // CPF válido
+}
